3e7f1c2ae3af8a5518c88ace6e3d3b06aa9eb204
guilhermealmeida123 <105080670+guilhermealmeida123@users.noreply.github.com>
2024-10-29 07:53:38 -0300

    ok

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,6 @@
+async function quantiddadeUsuarios() {
+    const url = 'https://raw.
+    githubusercontent.com/guilhermeonrails/api/main/numero-quantiddadeUsuarios.json1'
+    const res = await fetch(url)
+    const dados = await res.json()
+}
